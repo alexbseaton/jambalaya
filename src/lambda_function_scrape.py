b2db22ac0fb1ef5e8f694bf6058a125f649b86d6
owen.riddall <owen.riddall@oliverwyman.com>
2017-12-13 08:31:51 +0000

    Added lambda function script. Currently not working.

--- src/lambda_function_scrape.py
+++ src/lambda_function_scrape.py
@@ -1,10 +1,40 @@
+from datetime import datetime
+from urllib.request import urlopen
 import requests
 import json
-import urllib3
-from bs4 import BeautifulSoup
+from lxml import html
 import os
-import pickle as pkl
 import datetime as dt
+
+SITE = os.environ['site']  # URL of the site to check, stored in the site environment variable
+EXPECTED = os.environ['expected']  # String expected to be on the page, stored in the expected environment variable
+
+
+def validate(res):
+    '''Return False to trigger the canary
+
+    Currently this simply checks whether the EXPECTED string is present.
+    However, you could modify this to perform any number of arbitrary
+    checks on the contents of SITE.
+    '''
+    return EXPECTED in res
+
+
+def lambda_handler(event, context):
+    print('Checking {} at {}...'.format(SITE, event['time']))
+    try:
+        if not validate(str(urlopen(SITE).read())):
+            raise Exception('Validation failed')
+    except:
+        print('Check failed!')
+        raise
+    else:
+        print('Check passed!')
+
+        parse('LGW', 'MAD', '12/19/2017', test=False)
+        return event['time']
+    finally:
+        print('Check complete at {}'.format(str(datetime.now())))
 
 
 def parse(source, destination, date, test=False):
@@ -15,16 +45,12 @@ def parse(source, destination, date, test=False):
             timestamp = dt.datetime(2017, 12, 19, 17) if test else dt.datetime.now()
             timestamp = timestamp.strftime("%d-%m-%y_%H_%M")
             flight_data = (timestamp, json.loads(raw_json["content"])['legs'])
-            filename = 'scrape_' + str(timestamp)
-            filepath = os.path.join(os.pardir, 'data', 'scraped_data', filename)
-            with open(filepath, 'wb') as f:
-                pkl.dump(flight_data, f)
 
             print(flight_data)
             return
 
         except ValueError:
-            print('Attempt {} of {} failed. Retrying...'.format(i+1, n_tries))
+            print('Attempt {} of {} failed. Retrying...'.format(i + 1, n_tries))
 
 
 def get_raw_json(source, destination, date, test):
@@ -35,9 +61,8 @@ def get_raw_json(source, destination, date, test):
         url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:{0},to:{1},departure:{2}TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com".format(
             source, destination, date)
         print(url)
-        #response = requests.get(url)
-        response = urllib3.urlopen(url)
-        parser = BeautifulSoup(response, 'html.parser')
+        response = requests.get(url)
+        parser = html.fromstring(response.text)
         json_data_xpath = parser.xpath("//script[@id='cachedResultsJson']//text()")
         raw_json = json.loads(json_data_xpath[0])
 
@@ -47,7 +72,3 @@ def get_raw_json(source, destination, date, test):
 def save_json(raw_json):
     with open(os.path.join(os.pardir, 'data', 'test_data_130e4a71ac38db2b22c866088d5fe135'), 'w') as f:
         json.dump(raw_json, f)
-
-
-if __name__ == '__main__':
-    parse('LGW', 'MAD', '12/19/2017', test=False)
