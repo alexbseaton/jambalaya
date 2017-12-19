2390ea3f472759d4ee78c514c1e0851f28374e0b
flyingredd <flyingredd@yahoo.co.uk>
2017-12-19 22:29:10 +0000

    Move to beautiful soup
    (the reCapatcha is a big problem, TBD...)

--- src/scrape.py
+++ src/scrape.py
@@ -1,10 +1,10 @@
 import requests
 import json
-from lxml import html
+from bs4 import BeautifulSoup
 import os
 import pickle as pkl
 import datetime as dt
-
+import requests
 
 def parse(source, destination, date, test=False):
     n_tries = 1 if test else 5
@@ -14,8 +14,7 @@ def parse(source, destination, date, test=False):
             timestamp = dt.datetime(2017, 12, 19, 17) if test else dt.datetime.now()
             timestamp = timestamp.strftime("%d-%m-%y_%H_%M")
             flight_data = (timestamp, json.loads(raw_json["content"])['legs'])
-            filename = 'scrape_' + str(timestamp)
-            filepath = os.path.join(os.pardir, 'data', 'scraped_data', filename)
+            filepath = os.path.join(os.pardir, 'data', 'scraped_data', 'scrape_' + str(timestamp))
             with open(filepath, 'wb') as f:
                 pkl.dump(flight_data, f)
 
@@ -26,26 +25,17 @@ def parse(source, destination, date, test=False):
             print('Attempt {} of {} failed. Retrying...'.format(i+1, n_tries))
 
 
-def get_raw_json(source, destination, date, test):
+def get_raw_json(source, destination, date, test=False):
     if test:
-        with open(os.path.join(os.pardir, 'data', 'test_data', 'test_data_130e4a71ac38db2b22c866088d5fe135'), 'r') as f:
-            raw_json = json.load(f)
+        page = open(os.path.join(os.pardir, 'data', 'test_data', 'example_page_source.html'))
     else:
         url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:{0},to:{1},departure:{2}TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com".format(
             source, destination, date)
-        print(url)
-        response = requests.get(url)
-        parser = html.fromstring(response.text)
-        json_data_xpath = parser.xpath("//script[@id='cachedResultsJson']//text()")
-        raw_json = json.loads(json_data_xpath[0])
-
-    return raw_json
-
-
-def save_json(raw_json):
-    with open(os.path.join(os.pardir, 'data', 'test_data_130e4a71ac38db2b22c866088d5fe135'), 'w') as f:
-        json.dump(raw_json, f)
+        page = requests.get(url).text
+    
+    soup = BeautifulSoup(page, 'html.parser')
+    return json.loads(soup.find(id="cachedResultsJson").string)
 
 
 if __name__ == '__main__':
-    parse('LGW', 'MAD', '2/19/2018')
+    parse('LGW', 'MAD', '2/17/2018', True)
