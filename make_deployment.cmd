c39caaf14d156f28eae4f1c0f020d1621c2004e6
owen.riddall <owen.riddall@oliverwyman.com>
2017-12-25 11:42:26 +0000

    Added automated environment pinning.

--- make_deployment.cmd
+++ make_deployment.cmd
@@ -8,8 +8,8 @@ DEL deployment-zip.zip
 ECHO.
 
 ECHO Create deployment directory
-pip-compile --output-file deployment_requirements.txt deployment_requirements.in
-pip install -r deployment_requirements.txt -t deployment
+pip-compile --output-file .deployment_requirements.txt deployment_requirements.in
+pip install -r .deployment_requirements.txt -t deployment
 xcopy src\handler.py deployment
 cd deployment
 "C:\Program Files\7-Zip\7z.exe" a ../deployment-zip.zip *
---
+++ sync-env.cmd
@@ -0,0 +1,5 @@
+pip install --upgrade pip-tools
+
+pip-compile --output-file .dev_requirements.txt dev_requirements.in
+
+pip-sync .dev_requirements.txt
