968bdc169530f860778597b1c7fb283db4d6a442
flyingredd <flyingredd@yahoo.co.uk>
2017-12-26 21:55:35 +0000

    Fill in the parser to grab the bits we care about

--- src/parser.py
+++ src/parser.py
@@ -1,19 +1,34 @@
 import downloader
 import pprint
 import pickle as pkl
+from datetime import datetime
+
+
+def load_example_leg():
+    with open('../data/26-12-17_12_16.pkl', 'rb') as f:
+        pickle = pkl.load(f)
+    return pickle[0], next(iter(pickle[1].values()))
+
+
+def pull_numbers(time, leg):
+    dt = datetime.strptime(time, "%d-%m-%y_%H_%M")
+    print("Time of request: {}".format(dt))
+    print(leg['price']['totalPriceAsDecimal'])
+    print(leg['carrierSummary'])
+    print(leg['stops'])
+
 
 def create_dev_aids():
     '''
     Writes the key/value structure of a 'leg' to some text files in the ../data folder to make it a bit clearer
     what's going on.
     '''
-    with open('../data/26-12-17_12_16.pkl', 'rb') as f:
-        json = pkl.load(f)[1]
-    first_leg = next(iter(json.values()))
+    first_leg = load_example_leg()
     with open('../data/key_structure.txt', 'w') as f:
         f.write(visualise(first_leg, 1))
     with open('../data/value_structure.txt', 'w') as f:
         pprint.pprint(first_leg, stream=f)
+
 
 def visualise(dict_of_dicts: dict, n_nestings: int) -> str:
     result = ''
@@ -27,4 +42,5 @@ def visualise(dict_of_dicts: dict, n_nestings: int) -> str:
 
 
 if __name__ == '__main__':
-    create_dev_aids()
+    eg = load_example_leg()
+    pull_numbers(eg[0], eg[1])
