69f02a9df95bff8c23865fe97b7d9dc8b8fc8e9e
flyingredd <flyingredd@yahoo.co.uk>
2017-12-26 13:13:22 +0000

    Add starting point for the downloader. Maybe we can do a similar thing in the uploader so we needn't write to tmp ?

--- src/downloader.py
+++ src/downloader.py
@@ -1,5 +1,20 @@
+import boto3
+import io
+import pickle as pkl
+
 def download():
-    print("ran")
+    ''' 
+    Probably want the API to be something like given this date, return me a list of all the
+    things we saved that day, seeing as we won't know the exact time.
+    '''
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('alex-jambalaya-json-dumps')
+    key = 'scrape_26-12-17_12_38.pkl'
+    write_to = io.BytesIO()
+    bucket.download_fileobj(key, write_to)
+    write_to.seek(0) # got to drag the stream back to the beginning
+    return pkl.load(write_to)
+
 
 if __name__ == '__main__':
-    download()
+    print(download())
