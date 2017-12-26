eb0bba03d234f12bd810d71f67f9cb02abe68af6
owen.riddall <owen.riddall@oliverwyman.com>
2017-12-26 13:05:06 +0000

    Finished first pass at handler refactor.

--- src/handler.py
+++ src/handler.py
@@ -14,26 +14,26 @@ def my_handler(event, context):
     n_tries = event['n_tries']
     scrape(n_tries)
 
-
 def scrape(n_tries):
     for i in range(n_tries):
         try:
             raw_json = get_raw_json()
             flight_data = (dt.datetime.now().strftime("%d-%m-%y_%H_%M"), json.loads(raw_json["content"])['legs'])
             save_flight_data(flight_data, S3BUCKET)
-
             print(flight_data)
 
             return {'message' : "ran OK"}
+        except ValueError:
+            print('Attempt {} of {} failed. Retrying...'.format(i+1, n_tries))
 
 def get_raw_json():
-    url =   "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:"\
-            "{0},to:{1},departure:{2}TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3A"\
-            "economy&mode=search&origref=www.expedia.com".format('LGW', 'MAD', '2/17/2018')
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, 'html.parser')
+        url =   "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:"\
+                "{0},to:{1},departure:{2}TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3A"\
+                "economy&mode=search&origref=www.expedia.com".format('LGW', 'MAD', '2/17/2018')
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, 'html.parser')
 
-    return json.loads(soup.find(id="cachedResultsJson").string)
+        return json.loads(soup.find(id="cachedResultsJson").string)
 
 def save_flight_data(flight_data, s3_bucket):
     filename = "scrape_{}.pkl".format(flight_data[0])
