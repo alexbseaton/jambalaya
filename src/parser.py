73f017522579827bd840df848e87ff227d722d1e
flyingredd <flyingredd@yahoo.co.uk>
2017-12-27 13:51:49 +0000

    Revert "Revert "Pull out relevant data from the big JSON file""
    This reverts commit 60dbdb02a966300dd07fcf1bad7dc3310690fbbe.

--- src/parser.py
+++ src/parser.py
@@ -2,20 +2,52 @@ import downloader
 import pprint
 import pickle as pkl
 from datetime import datetime
+from datetime import timedelta
+import dateutil.parser
+from enum import Enum, unique
 
 
-def load_example_leg():
+@unique
+class TimeOfDay(Enum):
+    morning = 'MORNING'
+    afternoon = 'AFTERNOON'
+    evening = 'EVENING'
+
+    @staticmethod
+    def from_string(string):
+        for d in TimeOfDay:
+            if d.value == string:
+                return d
+        raise ValueError("Unknown time of day: {}".format(string))
+
+
+def load_example_legs():
     with open('../data/26-12-17_12_16.pkl', 'rb') as f:
         pickle = pkl.load(f)
-    return pickle[0], next(iter(pickle[1].values()))
+    return pickle[0], iter(pickle[1].values())
 
 
 def pull_numbers(time, leg):
-    dt = datetime.strptime(time, "%d-%m-%y_%H_%M")
-    print("Time of request: {}".format(dt))
-    print(leg['price']['totalPriceAsDecimal'])
-    print(leg['carrierSummary'])
-    print(leg['stops'])
+    request_time = datetime.strptime(time, "%d-%m-%y_%H_%M")
+    price = float(leg['price']['totalPriceAsDecimal'])
+    n_stops = int(leg['stops'])
+    # Always a list, in case there are multiple stops
+    airline_codes = leg['carrierSummary']['airlineCodes'] # https://en.wikipedia.org/wiki/List_of_airline_codes
+    departure_location = leg['departureLocation']['airportCode']
+    arrival_location = leg['arrivalLocation']['airportCode']
+
+    d = leg['departureTime']['isoStr']
+    departure_date = dateutil.parser.parse(d)
+
+    duration = leg['duration']
+    delta = timedelta(hours=duration['hours'], minutes=duration['minutes'])
+
+    departure_time_of_day = TimeOfDay.from_string(duration['departureTimeOfDay'])
+    arrival_time_of_day = TimeOfDay.from_string(duration['arrivalTimeOfDay'])
+
+    return {'request time': request_time, 'price': price, 'n_stops': n_stops, 'carriers': airline_codes,
+        'departure_location': departure_location, 'arrival_location': arrival_location, 'departure_date': departure_date,
+        'duration': delta, 'departure_time_of_day': departure_time_of_day, 'arrival_time_of_day': arrival_time_of_day}
 
 
 def create_dev_aids():
@@ -23,7 +55,7 @@ def create_dev_aids():
     Writes the key/value structure of a 'leg' to some text files in the ../data folder to make it a bit clearer
     what's going on.
     '''
-    first_leg = load_example_leg()
+    first_leg = next(load_example_legs()[1])
     with open('../data/key_structure.txt', 'w') as f:
         f.write(visualise(first_leg, 1))
     with open('../data/value_structure.txt', 'w') as f:
@@ -42,5 +74,6 @@ def visualise(dict_of_dicts: dict, n_nestings: int) -> str:
 
 
 if __name__ == '__main__':
-    eg = load_example_leg()
-    pull_numbers(eg[0], eg[1])
+    eg = load_example_legs()
+    for leg in filter(lambda leg: leg['stops'] == 0, eg[1]):
+        print(pull_numbers(eg[0], leg))
