00c3d0521975e24ec975a36c3e79f497ded8730a
flyingredd <flyingredd@yahoo.co.uk>
2017-12-28 19:33:50 +0000

    Whitespace

--- test/test_scrape.py
+++ test/test_scrape.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestScrape(unittest.TestCase):
     def test_scrape(self):
-        print(handler.scrape(5, 'alex-jambalaya-json-dumps'))
+        print(handler.scrape(5, 'alex-jambalaya-json-dumps', 'LGW', 'MAD', '1/17/2018'))
 
 if __name__ == '__main__':
     unittest.main()
