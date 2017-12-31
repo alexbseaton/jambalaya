05dec14c0779f13e86077270452e4c2961bea66d
flyingredd <flyingredd@yahoo.co.uk>
2017-12-31 15:15:38 +0000

    Connect to local database when running unit tests

--- test/test_scrape.py
+++ test/test_scrape.py
@@ -6,5 +6,8 @@ class TestScrape(unittest.TestCase):
     def test_scrape_parser(self):
         print(handler.scrape(5, 'alex-jambalaya-json-dumps', 'LGW', 'MAD', '1/17/2018'))
 
+    def test_handler(self):
+        handler.my_handler(None, None)
+
 if __name__ == '__main__':
     unittest.main()
