12d94079f6e360aa404c3755f6fcb3cf5d162c10
flyingredd <flyingredd@yahoo.co.uk>
2018-01-02 13:45:00 +0000

    Tidy up test

--- test/test_sql_alchemy.py
+++ test/test_sql_alchemy.py
@@ -16,23 +16,17 @@ class TestAlchemy(unittest.TestCase):
 
         print(alchemy_utils.Base.metadata.create_all(engine))
         Session = sessionmaker(bind=engine)
-        request_time = datetime.datetime(2018, 1, 2, 12, 16, 0)
-        price = 127.32
-        n_stops = 2
-        departure_location = 'LGW'
-        arrival_location = 'MAD'
-        departure_date = datetime.datetime(2018, 2, 2, 12, 16, 0)
-        duration = datetime.timedelta(hours=2, minutes=15)
-        leg = Leg(request_time=request_time, price=price, n_stops=n_stops, departure_location=departure_location\
-        ,arrival_location=arrival_location, departure_date=departure_date, duration=duration)
+        request_time = datetime.datetime(2018, 1, 3, 12, 16, 0)
+        leg = Leg(request_time=request_time, price=172.22, n_stops=2, departure_location='LGW'\
+        ,arrival_location='MAD', departure_date=datetime.datetime(2018, 2, 2, 12, 16, 0), duration=datetime.timedelta(hours=2, minutes=15))
 
         session = Session()
         session.add(leg)
 
-        our_leg = session.query(Leg).filter_by(request_time=request_time).first()
-        print(our_leg)
-
         session.commit()
+        loaded_leg = session.query(Leg).filter_by(request_time=request_time).first()
+
+        self.assertAlmostEqual(172.22, loaded_leg.price)
 
 
 if __name__ == '__main__':
