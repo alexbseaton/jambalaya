a1d62b263b8ebed13ee98d20001923efafd43306
flyingredd <flyingredd@yahoo.co.uk>
2018-01-02 13:33:07 +0000

    Explain how to ignore rds_config.py

--- src/rds_config.py
+++ src/rds_config.py
@@ -1,4 +1,6 @@
+# To ignore this, run git update-index --assume-unchanged src/rds_config.py
+# Can't add it to .gitignore since we want it in the repo... just not with our specific details.
 db_username = "admi"
-db_password = "password"
+db_password = "?"
 db_name = "jambalaya"
 rds_host  = "jambalaya-db.c0ajtn4w9cbm.eu-west-2.rds.amazonaws.com"
