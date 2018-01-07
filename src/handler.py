71b9a585774530aefd158aa95c519d6d732fd2e8
flyingredd <flyingredd@yahoo.co.uk>
2018-01-07 15:02:00 +0000

    Add sanity check that the departure is actually the one we care about

--- src/handler.py
+++ src/handler.py
@@ -34,21 +34,24 @@ def scrape(n_tries, departure_airport, arrival_airport, departure_date):
             legs = json.loads(raw_json["content"])['legs']
             if legs == {}:
                 raise ValueError("No data in script - maybe it sent us to a reCaptcha?")
-            persist_legs(legs)
+            persist_legs(legs, departure_date)
             return
         except ValueError:
             print('Attempt {} of {} failed. Retrying...'.format(i+1, n_tries))
 
 
-def persist_legs(legs):
+def persist_legs(legs, departure_date):
     request_time = dt.datetime.now()
     session = Session()
+    all_legs = [leg.create_leg(request_time, leg_json) for leg_json in legs.values()]
+    
+    # Sanity check- the stuff we're saving should have the departure date we asked for
+    for l in all_legs:
+        print('Departure date: \n{}\n\n'.format(l.departure_date))
+        print('Expected:{}'.format(departure_date))
+        assert l.departure_date.date() == departure_date.date()
+    
     try:
-        all_legs = [leg.create_leg(request_time, leg_json) for leg_json in legs.values()]
-        
-        for l in all_legs:
-            print('Departure date: \n{}\n\n'.format(l.departure_date))
-        
         session.add_all([l for l in all_legs if l.n_stops == 0])
         session.commit()
     except:
@@ -59,9 +62,10 @@ def persist_legs(legs):
 
 
 def get_raw_json(departure_airport, arrival_airport, departure_date):
+    mmddyyyy_date = departure_date.strftime('%m/%d/%Y')
     url =   "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:"\
             "{0},to:{1},departure:{2}TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3A"\
-            "economy&mode=search&origref=www.expedia.com".format(departure_airport, arrival_airport, departure_date)
+            "economy&mode=search&origref=www.expedia.com".format(departure_airport, arrival_airport, mmddyyyy_date)
     
     print('url:\n{}\n\n'.format(url))
     
@@ -70,5 +74,13 @@ def get_raw_json(departure_airport, arrival_airport, departure_date):
     return json.loads(soup.find(id="cachedResultsJson").string)
 
 
+def main():
+    departure_airport = 'LGW'
+    arrival_airport = 'MAD'
+    n_tries = 3
+    day_count = 1
+    for departure_date in (dt.datetime.now() + dt.timedelta(days=n+7) for n in range(day_count)):
+        scrape(n_tries, departure_airport, arrival_airport, departure_date)
+
 if __name__ == '__main__':
-    scrape(1, 'LGW', 'MAD', '2/2/2018')
+    main()
