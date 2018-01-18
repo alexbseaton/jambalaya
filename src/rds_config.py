dba52c911d2c1bbf34e057a7126f329d4d29bb80
owen.riddall <owen.riddall@oliverwyman.com>
2018-01-18 21:11:01 +0000

    Minor tidy up.

--- src/rds_config.py
+++ src/rds_config.py
@@ -3,4 +3,4 @@
 db_username = "admi"
 db_password = "?"
 db_name = "jambalaya"
-rds_host  = "jambalaya-db.c0ajtn4w9cbm.eu-west-2.rds.amazonaws.com"
+rds_host = "jambalaya-db.c0ajtn4w9cbm.eu-west-2.rds.amazonaws.com"
